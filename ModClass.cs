f620ec2f2b7b520c303d01eec609aa1094bd8766
Thommie <T.C.vanderwind@student.tudelft.nl>
2026-02-11 17:11:21 +0100

    Added free gathering swarm, working on the regenerate health brick

--- ModClass.cs
+++ ModClass.cs
@@ -3,14 +3,13 @@ using Modding;
 using UnityEngine;
 using SFCore;
 using Satchel;
+using JetBrains.Annotations;
+using System.Threading.Tasks;
 
 namespace Lego_Power_Bricks
 {
-    /*Bricks to add
-    - x4 Geo multiplier
-    - x6 Geo multiplier
-    - x8 Geo multiplier
-    - x10 Geo multiplier
+    /*To do list:
+    - Add current multiplier next to geo count
     - Geo magnet
     - Regenerate hp
     - Increase hp
@@ -23,7 +22,7 @@ namespace Lego_Power_Bricks
     public class x2Multiplier : EasyCharm
     {
         protected override int GetCharmCost() => 0;
-        protected override string GetDescription() => "Multiplies geo worth by two";
+        protected override string GetDescription() => "Multiplies geo worth by 2";
         protected override string GetName() => "Geo x2";
         protected override Sprite GetSpriteInternal() => AssemblyUtils.GetSpriteFromResources("Red_brick.png");
     }
@@ -31,7 +30,7 @@ namespace Lego_Power_Bricks
     public class x4Multiplier : EasyCharm
     {
         protected override int GetCharmCost() => 0;
-        protected override string GetDescription() => "Multiplies geo worth by four";
+        protected override string GetDescription() => "Multiplies geo worth by 4";
         protected override string GetName() => "Geo x4";
         protected override Sprite GetSpriteInternal() => AssemblyUtils.GetSpriteFromResources("Red_brick.png");
     }
@@ -39,7 +38,7 @@ namespace Lego_Power_Bricks
     public class x6Multiplier : EasyCharm
     {
         protected override int GetCharmCost() => 0;
-        protected override string GetDescription() => "Multiplies geo worth by six";
+        protected override string GetDescription() => "Multiplies geo worth by 6";
         protected override string GetName() => "Geo x6";
         protected override Sprite GetSpriteInternal() => AssemblyUtils.GetSpriteFromResources("Red_brick.png");
     }
@@ -47,7 +46,7 @@ namespace Lego_Power_Bricks
     public class x8Multiplier : EasyCharm
     {
         protected override int GetCharmCost() => 0;
-        protected override string GetDescription() => "Multiplies geo worth by eight";
+        protected override string GetDescription() => "Multiplies geo worth by 8";
         protected override string GetName() => "Geo x8";
         protected override Sprite GetSpriteInternal() => AssemblyUtils.GetSpriteFromResources("Red_brick.png");
     }
@@ -60,6 +59,21 @@ namespace Lego_Power_Bricks
         protected override Sprite GetSpriteInternal() => AssemblyUtils.GetSpriteFromResources("Red_brick.png");
     }
 
+    public class GeoMagnet : EasyCharm
+    {
+        protected override int GetCharmCost() => 0;
+        protected override string GetDescription() => "Makes gathering swarm free";
+        protected override string GetName() => "Geo Magnet";
+        protected override Sprite GetSpriteInternal() => AssemblyUtils.GetSpriteFromResources("Red_brick.png");
+    }
+
+    public class RegenerateHeatlh : EasyCharm
+    {
+        protected override int GetCharmCost() => 0;
+        protected override string GetDescription() => "Regenerates health";
+        protected override string GetName() => "Regenerates health";
+        protected override Sprite GetSpriteInternal() => AssemblyUtils.GetSpriteFromResources("Red_brick.png");
+    }
 
     public class Settings
     {
@@ -75,15 +89,77 @@ namespace Lego_Power_Bricks
             {"x4Multiplier", new x4Multiplier()},
             {"x6Multiplier", new x6Multiplier()},
             {"x8Multiplier", new x8Multiplier()},
-            {"x10Multiplier", new x10Multiplier()}
+            {"x10Multiplier", new x10Multiplier()},
+            {"geoMagnet", new GeoMagnet()},
+            {"regenerateHealth", new RegenerateHeatlh()}
         };
 
 
         public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
         {
             On.HeroController.AddGeo += AddGeo;
+            //On.GameManager.CalculateNotchesUsed += OnCalculateNotches;
+            ModHooks.CharmUpdateHook += OnCharmUpdate;
+            ModHooks.HeroUpdateHook += OnHeroUpdate;
         }
+        public void OnHeroUpdate()
+        {
+            //if (PlayerData.instance.health < PlayerData.instance.maxHealth && Charms["regenerateHealth"].IsEquipped)
+            //{
+            //    Log("PlayerHealth is lower, run Add Health");
+            //    AddHealth(); // Explicitly discard the returned Task to suppress CS4014
+            //}
+        }
+        public void OnCharmUpdate(PlayerData data, HeroController controller)
+        {
+            Log($"OnCharmUpdate called with data: {data} and {controller}");
+            if (Charms["geoMagnet"].IsEquipped)
+            {
+                data.CalculateNotchesUsed();
+                data.charmCost_1 = 0;
+            }
+            else
+            {
+                data.CalculateNotchesUsed();
+                data.charmCost_1 = 1;
+            }
+            if (!Charms["geoMagnet"].IsEquipped && data.equippedCharm_1)
+            {
+                data.CalculateNotchesUsed();
+            }
+        }
+        //async Task AddHealth()
+        //{
+        //    if (!Charms["regenerateHealth"].IsEquipped) return;
+        //    await Task.Delay(5000); // Wait for 5 seconds before regenerating health
+        //    PlayerData.instance.health++;
+        //}
 
+        public void AddGeo(On.HeroController.orig_AddGeo orig, HeroController self, int amount)
+        {
+            if (Charms["x2Multiplier"].IsEquipped)
+            {
+                amount *= 2; // Double the amount of geo gained
+            }
+            if (Charms["x4Multiplier"].IsEquipped)
+            {
+                amount *= 4; // Quadruple the amount of geo gained
+            }
+            if (Charms["x6Multiplier"].IsEquipped)
+            {
+                amount *= 6; // Sextuple the amount of geo gained
+            }
+            if (Charms["x8Multiplier"].IsEquipped)
+            {
+                amount *= 8; // Octuple the amount of geo gained
+            }
+            if (Charms["x10Multiplier"].IsEquipped)
+            {
+                amount *= 10; // Decuple the amount of geo gained
+            }
+            // Check if the player has the x2 multiplier charm equipped
+            orig(self, amount);
+        }
         public void OnLoadLocal(Settings s)
         {
             localSettings = s;
@@ -109,31 +185,6 @@ namespace Lego_Power_Bricks
                 }
             }
             return localSettings;
-        }
-        public void AddGeo(On.HeroController.orig_AddGeo orig, HeroController self, int amount)
-        {
-            if (Charms["x2Multiplier"].IsEquipped)
-            {
-                amount *= 2; // Double the amount of geo gained
-            }
-            if (Charms["x4Multiplier"].IsEquipped)
-            {
-                amount *= 4; // Quadruple the amount of geo gained
-            }
-            if (Charms["x6Multiplier"].IsEquipped)
-            {
-                amount *= 6; // Sextuple the amount of geo gained
-            }
-            if (Charms["x8Multiplier"].IsEquipped)
-            {
-                amount *= 8; // Octuple the amount of geo gained
-            }
-            if (Charms["x10Multiplier"].IsEquipped)
-            {
-                amount *= 10; // Decuple the amount of geo gained
-            }
-            // Check if the player has the x2 multiplier charm equipped
-            orig(self, amount);
         }
     }
 
